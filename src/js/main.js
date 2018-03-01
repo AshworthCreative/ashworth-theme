9419bbed3e74792b65c2d70eb6c8631c3b442c42
Luis Amador <mrluisamador@gmail.com>
2018-02-28 22:10:17 -0500

    adding the assets files and folder to the theme

---
+++ src/js/main.js
@@ -0,0 +1,15 @@
+/*
+* Adds CSS into your page.
+* It's required and follow the stacked order
+*
+*/
+
+// All of bootstrap
+import "bootstrap";
+
+// Alternatively, you may import plugins individually as needed:
+// import 'bootstrap/js/dist/dropdown';
+
+import "../css/main.scss";
+
+import "../img/example.png";
