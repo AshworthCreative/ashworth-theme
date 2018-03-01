edc1c7a67eda751a1093f736d8585340c4968283
Luis Amador <mrluisamador@gmail.com>
2018-02-28 22:39:27 -0500

    enqueue js and css in function.php file

--- functions.php
+++ functions.php
@@ -117,11 +117,13 @@ add_action( 'widgets_init', 'ashworth_widgets_init' );
  * Enqueue scripts and styles.
  */
 function ashworth_scripts() {
-	wp_enqueue_style( 'ashworth-style', get_stylesheet_uri() );
+	wp_enqueue_style( 'ashworth-style', get_template_directory_uri() . '/assets/css/main.css' );
 
 	wp_enqueue_script( 'ashworth-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
 	wp_enqueue_script( 'ashworth-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+
+	wp_enqueue_script( 'ashworth-js', get_template_directory_uri() . '/assets/js/bundle.js', array( 'jquery' ), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
@@ -155,4 +157,3 @@ require get_template_directory() . '/inc/customizer.php';
 if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
-
